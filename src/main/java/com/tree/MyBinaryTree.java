3428305ed5dbc07315c67d659e7beae19a4edf0f
biswajitdixit <buntyoec1998@gmail.com>
2021-02-09 19:26:01 +0530

    ADDED: search 63 in binary tree

--- src/main/java/com/tree/MyBinaryTree.java
+++ src/main/java/com/tree/MyBinaryTree.java
@@ -2,6 +2,8 @@ package com.tree;
 
 public class MyBinaryTree<K extends Comparable<K>> {
     private MyBinaryNode<K> root;
+    private boolean resultToBeReturned = false;
+
 
     public void add(K key) {
         this.root = this.addRecursively(root, key);
@@ -28,6 +30,31 @@ public class MyBinaryTree<K extends Comparable<K>> {
 
     private int getSizeRecursive(MyBinaryNode<K> current) {
         return current == null ? 0 : 1 + this.getSizeRecursive(current.left) + this.getSizeRecursive(current.right);
+    }
+    public boolean search(K key) {
+        boolean result = this.searchRecursively(this.root, key);
+        if (resultToBeReturned == true) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean searchRecursively(MyBinaryNode<K> current, K key) {
+        if (current == null) {
+            return false;
+        }
+
+        int compareResult = key.compareTo(current.key);
+        if (compareResult == 0) {
+            this.resultToBeReturned = true;
+            return true;
+        }
+        if (compareResult < 0) {
+            searchRecursively(current.left, key);
+        } else {
+            searchRecursively(current.right, key);
+        }
+        return false;
     }
 
 }
